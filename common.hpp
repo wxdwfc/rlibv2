c90d3220fff49ee1dfb1c7220ca2b135ccdb51a3
wxd <wxdwfc@gmail.com>
2019-08-02 20:02:27 +0800

    minor code layout

--- common.hpp
+++ common.hpp
@@ -10,21 +10,23 @@
 
 #include "logging.hpp"
 
-namespace rdmaio {
+namespace rdmaio
+{
 
 // some constants definiations
 // connection status
-enum IOStatus {
-  SUCC         = 0,
-  TIMEOUT      = 1,
-  WRONG_ARG    = 2,
-  ERR          = 3,
-  NOT_READY    = 4,
-  UNKNOWN      = 5,
-  WRONG_ID     = 6,
-  WRONG_REPLY  = 7,
-  NOT_CONNECT  = 8,
-  EJECT        = 9,
+enum IOStatus
+{
+  SUCC = 0,
+  TIMEOUT = 1,
+  WRONG_ARG = 2,
+  ERR = 3,
+  NOT_READY = 4,
+  UNKNOWN = 5,
+  WRONG_ID = 6,
+  WRONG_REPLY = 7,
+  NOT_CONNECT = 8,
+  EJECT = 9,
   REPEAT_CREATE = 10
 };
 
@@ -35,15 +37,17 @@ enum IOStatus {
  * So only simple function request handling is supported.
  * For example, we use this to serve the QP and MR information to other nodes.
  */
-enum RESERVED_REQ_ID {
+enum RESERVED_REQ_ID
+{
   REQ_RC = 0,
   REQ_UD = 1,
   REQ_UC = 2,
   REQ_MR = 3,
-  FREE   = 4
+  FREE = 4
 };
 
-enum {
+enum
+{
   MAX_INLINE_SIZE = 64
 };
 
@@ -51,30 +55,35 @@ enum {
  * We use TCP/IP to identify the machine,
  * since RDMA requires an additional naming mechanism.
  */
-typedef std::tuple<std::string,int> MacID;
-inline MacID make_id(const std::string &ip,int port) {
-  return std::make_tuple(ip,port);
+typedef std::tuple<std::string, int> MacID;
+inline MacID make_id(const std::string &ip, int port)
+{
+  return std::make_tuple(ip, port);
 }
 
-class QPDummy {
- public:
-  bool valid() const {
+class QPDummy
+{
+public:
+  bool valid() const
+  {
     return qp_ != nullptr && cq_ != nullptr;
   }
-  struct ibv_qp  *qp_ = nullptr;
-  struct ibv_cq *cq_  = nullptr;
+  struct ibv_qp *qp_ = nullptr;
+  struct ibv_cq *cq_ = nullptr;
   struct ibv_cq *recv_cq_ = nullptr;
 }; // a placeholder for a dummy class
 
-typedef struct {
+typedef struct
+{
   uint64_t subnet_prefix;
   uint64_t interface_id;
   uint32_t local_id;
 } qp_address_t;
 
-struct QPAttr {
-  QPAttr(const qp_address_t &addr,uint64_t lid,uint64_t psn,uint64_t port_id,uint64_t qpn = 0,uint64_t qkey = 0):
-      addr(addr),lid(lid),qpn(qpn),psn(psn),port_id(port_id){
+struct QPAttr
+{
+  QPAttr(const qp_address_t &addr, uint64_t lid, uint64_t psn, uint64_t port_id, uint64_t qpn = 0, uint64_t qkey = 0) : addr(addr), lid(lid), qpn(qpn), psn(psn), port_id(port_id)
+  {
   }
   QPAttr() {}
   qp_address_t addr;
--- rdma_ctrl.hpp
+++ rdma_ctrl.hpp
@@ -6,28 +6,43 @@
 #include <functional>
 #include <pthread.h>
 
-namespace rdmaio {
+namespace rdmaio
+{
 
 class RdmaCtrl
 {
+  // registered services
+  std::map<int, req_handler_f> registered_handlers;
+
+  bool running_ = false;
+  pthread_t handler_tid_;
+  std::mutex lock;
+
+public:
+  MacID host_id_;
+  RMemoryFactory mr_factory;
+  QPFactory qp_factory;
+
 public:
   RdmaCtrl() = default;
 
-  RdmaCtrl(int tcp_port, const std::string& ip = "localhost")
-    : lock()
+  RdmaCtrl(int tcp_port, const std::string &ip = "localhost")
+      : lock()
   {
     bind(tcp_port, ip);
   }
 
-  void bind(int tcp_port, const std::string& ip = "localhost")
+  void bind(int tcp_port, const std::string &ip = "localhost")
   {
     { // sanity check to avoid creating multiple threads
       std::lock_guard<std::mutex> lk(this->lock);
-      if (running_) {
+      if (running_)
+      {
         RDMA_LOG(4) << "warning, RDMA ctrl has already binded to " << ip << ":"
                     << tcp_port;
         return;
-      } else
+      }
+      else
         running_ = true;
     }
     host_id_ = std::make_tuple(ip, tcp_port);
@@ -43,8 +58,6 @@ public:
                                  std::bind(&QPFactory::get_ud_handler,
                                            &qp_factory,
                                            std::placeholders::_1)));
-
-    RDMA_LOG(2) << "pthread started";
     // start the listener thread
     pthread_attr_t attr;
     pthread_attr_init(&attr);
@@ -54,48 +67,36 @@ public:
   ~RdmaCtrl()
   {
     std::lock_guard<std::mutex> lk(this->lock);
-    if (running_) {
+    if (running_)
+    {
       running_ = false; // wait for the handler to join
       pthread_join(handler_tid_, NULL);
       RDMA_LOG(INFO)
-        << "rdma controler close: does not handle any future connections.";
+          << "rdma controler close: does not handle any future connections.";
     }
   }
 
-  typedef std::function<Buf_t(const Buf_t& req)> req_handler_f;
+  using req_handler_f = std::function<Buf_t(const Buf_t &req)>;
   bool register_handler(int rid, req_handler_f f)
   {
     return check_with_insert(rid, registered_handlers, f);
   }
 
-public:
-  MacID host_id_;
-  RMemoryFactory mr_factory;
-  QPFactory qp_factory;
-
-private:
-  // registered services
-  std::map<int, req_handler_f> registered_handlers;
-
-private:
-  bool running_ = false;
-  pthread_t handler_tid_;
-  std::mutex lock;
-
-  template<typename T>
-  bool check_with_insert(int id, std::map<int, T>& m, const T& val)
+  template <typename T>
+  bool check_with_insert(int id, std::map<int, T> &m, const T &val)
   {
     std::lock_guard<std::mutex> lk(this->lock);
-    if (m.find(id) == m.end()) {
+    if (m.find(id) == m.end())
+    {
       m.insert(std::make_pair(id, val));
       return true;
     }
     return false;
   }
 
-  static void* listener_wrapper(void* context)
+  static void *listener_wrapper(void *context)
   {
-    return ((RdmaCtrl*)context)->req_handling_loop();
+    return ((RdmaCtrl *)context)->req_handling_loop();
   }
 
   /**
@@ -103,7 +104,7 @@ private:
    * This is not optimized, since we rarely we need the controler to
    * do performance critical jobs.
    */
-  void* req_handling_loop(void)
+  void *req_handling_loop(void)
   {
     RDMA_ASSERT(running_ = true);
     pthread_detach(pthread_self());
@@ -112,37 +113,42 @@ private:
 
     int opt = 1;
     RDMA_VERIFY(
-      ERROR,
-      setsockopt(
-        listenfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(int)) ==
-        0)
-      << "unable to configure socket status.";
+        ERROR,
+        setsockopt(
+            listenfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(int)) ==
+            0)
+        << "unable to configure socket status.";
     RDMA_VERIFY(ERROR, listen(listenfd, 24) == 0)
-      << "TCP listen error: " << strerror(errno);
+        << "TCP listen error: " << strerror(errno);
 
-    while (running_) {
+    while (running_)
+    {
 
-      asm volatile("" ::: "memory");
+      asm volatile("" ::
+                       : "memory");
 
-      struct sockaddr_in cli_addr = { 0 };
+      struct sockaddr_in cli_addr = {0};
       socklen_t clilen = sizeof(cli_addr);
-      auto csfd = accept(listenfd, (struct sockaddr*)&cli_addr, &clilen);
+      auto csfd = accept(listenfd, (struct sockaddr *)&cli_addr, &clilen);
 
-      if (csfd < 0) {
+      if (csfd < 0)
+      {
         RDMA_LOG(ERROR) << "accept a wrong connection error: "
                         << strerror(errno);
         continue;
       }
 
-      if (!PreConnector::wait_recv(csfd, default_timeout)) {
+      if (!PreConnector::wait_recv(csfd, default_timeout))
+      {
         close(csfd);
         continue;
       }
 
       {
         Buf_t buf = Marshal::get_buffer(4096);
-        auto n = recv(csfd, (char*)(buf.data()), 4096, 0);
-        if (n < sizeof(RequestHeader)) {
+        auto n = recv(csfd, (char *)(buf.data()), 4096, 0);
+        if (n < sizeof(RequestHeader))
+        {
           goto END;
         }
 
@@ -152,11 +158,11 @@ private:
             registered_handlers.end())
           goto END;
         auto reply = registered_handlers[header.req_type](
-          Marshal::forward(buf, sizeof(RequestHeader), header.req_payload));
+            Marshal::forward(buf, sizeof(RequestHeader), header.req_payload));
 
-        PreConnector::send_to(csfd, (char*)(reply.data()), reply.size());
+        PreConnector::send_to(csfd, (char *)(reply.data()), reply.size());
         PreConnector::wait_close(
-          csfd); // wait for the client to close the connection
+            csfd); // wait for the client to close the connection
       }
     END:
       close(csfd);
