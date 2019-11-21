5e2c77e78e6bbb277001f2d0260a4f3fa198d691
wxd <wxdwfc@gmail.com>
2019-11-21 16:09:58 +0800

    scheme for ud pingpong

--- examples/exp.cmake
+++ examples/exp.cmake
@@ -3,5 +3,9 @@ include_directories(./examples/gflags/include)
 add_executable(pclient "${CMAKE_SOURCE_DIR}/examples/rc_pingpong/client.cc")
 add_executable(pserver "${CMAKE_SOURCE_DIR}/examples/rc_pingpong/server.cc")
 
+add_executable(uclient "${CMAKE_SOURCE_DIR}/examples/ud_pingpong/client.cc")
+
 target_link_libraries(pclient -lpthread ibverbs gflags)
 target_link_libraries(pserver -lpthread ibverbs  gflags)
+
+target_link_libraries(uclient -lpthread ibverbs gflags)
