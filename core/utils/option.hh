724dc176cf29cff4f522e4ebfcc852290e7dffff
wxd <wxdwfc@gmail.com>
2020-11-27 14:41:18 +0800

    minor option naming

--- core/utils/option.hh
+++ core/utils/option.hh
@@ -2,13 +2,12 @@
 
 #if defined(__GNUC__) && __GNUC__ < 7
 #include <experimental/optional>
-#ifndef optional
-#define optional experimental::optional
-#endif
+#define _rlib_optional std::experimental::optional
 #else
 #include <optional>
+#define _rlib_optional std::optional
 #endif
 
 namespace rdmaio {
-template <typename T> using Option = std::optional<T>;
+template <typename T> using Option = _rlib_optional<T>;
 }
